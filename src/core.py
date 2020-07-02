f64ebf02e2e8a082f7eed76ee3c088d995f920aa
Jose Epifanio <jose.epifanio90@gmail.com>
2020-07-01 21:38:15 -0400

    Add basic setup with vizdoom

---
+++ src/core.py
@@ -0,0 +1,28 @@
+import os
+import random
+import time
+
+from vizdoom import *
+
+game = DoomGame()
+print(os.getcwd())
+game.load_config("./scenarios/basic.cfg")
+game.init()
+
+shoot = [0, 0, 1]
+left = [1, 0, 0]
+right = [0, 1, 0]
+actions = [shoot, left, right]
+
+episodes = 10
+for i in range(episodes):
+    game.new_episode()
+    while not game.is_episode_finished():
+        state = game.get_state()
+        img = state.screen_buffer
+        misc = state.game_variables
+        reward = game.make_action(random.choice(actions))
+        print("\treward:", reward)
+        time.sleep(0.02)
+    print("Result:", game.get_total_reward())
+    time.sleep(2)
